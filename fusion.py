2ab724059502bdc1f4efa6dbb6e7d43de7612a51
Yarmouth21 <jurgenstyll@icloud.com>
2025-11-25 00:55:05 +0100

    Création du csv fusionné

--- fusion.py
+++ fusion.py
@@ -8,3 +8,11 @@ tab = pd.merge(tab1,tab2, on="date")
 tab = tab.drop(columns=["Unnamed: 0_x", "Unnamed: 0_y"])
 
 print(tab.head())
+
+tab["RET"] = pd.to_numeric(tab["RET"], errors="coerce")
+
+filtre1 = tab[tab["RET"]>0.08]
+prop = len(filtre1)/len(tab)
+print("Pourcentage d'actions de la base avec un retour supérieur à 8% : "+ str(prop))
+
+tab.to_csv("Fusionné.csv")
